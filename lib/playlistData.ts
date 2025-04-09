f40a5de0ba57e3484b7506305717660ead5a9b3d
Pengnim <skim808@snu.ac.kr>
2025-04-09 10:59:46 +0900

    update playlistData.ts to add lyrics for multiple songs, enhancing content richness and user engagement

--- lib/playlistData.ts
+++ lib/playlistData.ts
@@ -533,18 +533,610 @@ Oh, for now and always
 'Til the end of my days
 You've got all my love
 You've got all my love`, youtubeId: "4NKhYkFAmUw" },
-  { id: 11, title: "Human Heart", lyrics: "", youtubeId: "4n6Cdzh-lY0" },
-  { id: 12, title: "People of the Pride", lyrics: "", youtubeId: "Q4nFTEsez5c" },
-  { id: 13, title: "Clocks", lyrics: "", youtubeId: "3A3hgfstCxE" },
-  { id: 14, title: "WE PRAY", lyrics: "", youtubeId: "62QAZotpBNk" },
-  { id: 15, title: "Infinity Sign", lyrics: "", youtubeId: "CNmL8icWWRg" },
-  { id: 16, title: "Something Just Like This", lyrics: "", youtubeId: "FM7MFYoylVs" },
-  { id: 17, title: "My Universe", lyrics: "", youtubeId: "m9BX2yn9524" },
-  { id: 18, title: "A Sky Full of... Stars", lyrics: "", youtubeId: "NDonh28AY3I" }, // Note: Title adjusted slightly for simplicity
+  { id: 11, title: "Human Heart", lyrics: `"♡"
+
+Boys
+Boys don't cry
+Boys keep it all inside
+I tried to hide it underneath
+Still, my heart starts to beat
+
+My human heart
+Only got a human heart
+I wish it didn't run away
+I wish it didn't fall apart
+
+Oh, my human heart`, youtubeId: "4n6Cdzh-lY0" },
+  { id: 12, title: "People of the Pride", lyrics: `People on the left
+People on the right
+You got a lion inside
+People are the pride
+Let's go
+
+There's a man who swears he's God
+Unbelievers will be shot
+There's a man who walks around
+Like he owns the fuckin' lot
+There's a man who takes his time
+From his homemade cuckoo clock
+And he makes us march around it
+Tick-tock, tick-tock, tick-tock
+
+There's a crocodile cross-eyed
+There's a turning of the tide
+We're no longer gonna fight for
+Some old crook and all his crimes
+There's a sewing up of rags
+Into revolution flags
+Got to stand up to be counted
+Be an anthem for your times
+
+It's just work
+It's just work, it's not easy
+And we could all be blown apart
+And Heaven is a fire escape
+You try to cling to in the dark
+It's just work, believe me
+Still, my beating heart
+We'll all be free to fall in love
+With who we want and say
+
+Yeah (Yeah)
+Yeah (Yeah) (Ooh)
+Yeah (Yeah)
+People of the pride (Go)
+Night and day and light and dark
+Any day, it could be torn in half, ooh
+Only got a human heart
+
+Girls can make-believe
+Girls wear it on the sleeve
+Thought I could pretend for trying's sake
+Still my heart had to break
+
+My human heart
+Only got a human heart
+I wish it didn't run away
+I wish it didn't fall apart
+
+Oh, my human heart
+Night and day and light and dark
+Any day, it could be torn in half, ooh
+Only got a human heart`, youtubeId: "Q4nFTEsez5c" },
+  { id: 13, title: "Clocks", lyrics: `The lights go out and I can't be saved
+Tides that I tried to swim against
+Have brought me down upon my knees
+Oh I beg, I beg and plead, singing
+
+Come out of things unsaid
+Shoot an apple off my head and a
+Trouble that can't be named
+A tiger's waiting to be tamed, singing
+
+You are
+You are
+
+Confusion never stops
+Closing walls and ticking clocks
+Gonna come back and take you home
+I could not stop that you now know, singing
+
+Come out upon my seas
+Cursed missed opportunities
+Am I a part of the cure?
+Or am I part of the disease? Singing
+
+You are, you are, you are
+You are, you are, you are
+
+And nothing else compares
+Oh nothing else compares
+And nothing else compares
+
+You are
+You are
+
+Home, home where I wanted to go
+Home, home where I wanted to go
+Home, home where I wanted to go
+Home, home where I wanted to go`, youtubeId: "3A3hgfstCxE" },
+  { id: 14, title: "WE PRAY", lyrics: `Woah
+Oh
+And so we pray
+Ooh
+
+I pray that I don't give up, pray that I do my best
+Pray that I can lift up, pray my brother is blessed
+Praying for enough, pray Virgilio wins
+Pray I judge nobody and forgive me my sins
+I pray we make it, pray my friend will pull through
+Pray as I take it unto others, I do
+Praying on your love, we pray with every breath
+Though I'm in the valley of the shadow of death
+
+And so we pray
+For someone to come and show me the way
+And so we pray
+For some shelter and some records to play
+And so we pray
+We'll be singing "Baraye"
+Pray that we make it to the end of the day
+And so we pray
+I know somewhere that Heaven is waitin'
+And so we pray
+I know somewhere there's something amazin'
+And so we pray
+I know somewhere we'll feel no pain
+Until we make it to the end of the day
+
+I pray that love will shelter us from our fears
+Oh, I pray you trust to let me wipe off your tears
+Confront all the pain that we felt inside
+With all the cards we been dealt in life
+Pray I speak my truth and keep my sisters alive
+So for the ones who parted seas (Ah, yeah)
+For the ones who's followin' dreams (Ah, yeah)
+For the ones who knocked down doors (Ah, yeah)
+And allowed us to pass down keys (Ah, yeah)
+Pray that we speak with a tongue that is honest
+And that we understand how to be modest
+Pray when she looks at herself in the mirror
+She sees a queen, she sees a goddess
+
+And so we pray
+For someone to come and show me the way
+And so we pray
+For some shelter and some records to play
+And so we pray
+We'll be singin' "Baraye"
+Pray that we make it to the end of the day
+And so we pray
+I know somewhere that Heaven is waitin'
+And so we pray
+I know somewhere there's something amazin'
+And so we pray
+I know somewhere we'll feel no pain
+Until we make it to the end of the day
+
+On my knees, I pray, as I sleep and wake
+'Cause inside my head is a frightening place
+Keep a smiling face, only by this grace
+'Cause love's more than I can take, hey
+
+And so we pray
+For someone to come and show me the way
+And so we pray
+For some shelter and some records to play
+And so we pray
+We'll be singin' "Baraye"
+Till nobody's in need and everybody can say (Oh)
+
+La-la-la, la-la, la-la (La-la)
+La-la-la, la-la, la-la (La-la-la, la)
+La-la-la, la-la, la-la (Sing it to me)
+La-la-la, la-la, we pray, we pray
+La-la-la, la-la, la-la (I know somewhere that Heaven is waitin', is waitin')
+La-la-la, la-la, la-la (I know somewhere there's something amazin', something amazin')
+La-la-la, la-la, la-la (Until we feel no pain)
+La-la-la, la-la, we pray, we pray
+
+La-la-la, la-la, la-la
+La-la-la, la-la, la-la
+La-la-la, la-la, la-la
+La-la-la, la-la, we pray, we pray, we pray`, youtubeId: "62QAZotpBNk" },
+  { id: 15, title: "∞", lyrics: "", youtubeId: "CNmL8icWWRg" },
+  { id: 16, title: "Something Just Like This", lyrics: `[Chris Martin:]
+I've been reading books of old
+The legends and the myths
+Achilles and his gold
+Hercules and his gifts
+Spiderman's control
+And Batman with his fists
+And clearly I don't see myself upon that list
+
+But she said, where'd you wanna go?
+How much you wanna risk?
+I'm not looking for somebody
+With some superhuman gifts
+Some superhero
+Some fairytale bliss
+Just something I can turn to
+Somebody I can kiss
+
+I want something just like this
+Doo-doo-doo, doo-doo-doo
+Doo-doo-doo, doo-doo
+Doo-doo-doo, doo-doo-doo
+Oh, I want something just like this
+Doo-doo-doo, doo-doo-doo
+Doo-doo-doo, doo-doo
+Doo-doo-doo, doo-doo-doo
+
+Oh, I want something just like this
+I want something just like this
+
+I've been reading books of old
+The legends and the myths
+The testaments they told
+The moon and its eclipse
+And Superman unrolls
+A suit before he lifts
+But I'm not the kind of person that it fits
+
+She said, where'd you wanna go?
+How much you wanna risk?
+I'm not looking for somebody
+With some superhuman gifts
+Some superhero
+Some fairytale bliss
+Just something I can turn to
+Somebody I can miss
+
+I want something just like this
+I want something just like this
+
+Oh, I want something just like this
+Doo-doo-doo, doo-doo-doo
+Doo-doo-doo, doo-doo
+Doo-doo-doo, doo-doo-doo
+Oh, I want something just like this
+Doo-doo-doo, doo-doo-doo
+Doo-doo-doo, doo-doo
+Doo-doo-doo, doo-doo-doo
+
+Where'd you wanna go?
+How much you wanna risk?
+I'm not looking for somebody
+With some superhuman gifts
+Some superhero
+Some fairytale bliss
+Just something I can turn to
+Somebody I can kiss
+I want something just like this
+
+Oh, I want something just like this
+Oh, I want something just like this
+Oh, I want something just like this`, youtubeId: "FM7MFYoylVs" },
+  { id: 17, title: "My Universe", lyrics: `You (you), you are (you are) my universe
+And I (I) just want (just want) to put you first
+And you (you), you are (you are) my universe, and I
+In the night, I lie and look up at you
+When the morning comes, I watch you rise
+There's a paradise they couldn't capture
+That bright infinity inside your eyes
+매일 밤 네게 날아가 (가)
+꿈이란 것도 잊은 채
+나 웃으며 너를 만나 (나)
+Never ending forever, baby
+You (you), you are (you are) my universe
+And I (I) just want (just want) to put you first
+And you (you), you are (you are) my universe
+And you make my world light up inside
+어둠이 내겐 더 편했었지
+길어진 그림자 속에서 (eyes)
+And they said that we can't be together
+Because, because we come from different sides
+You (you), you are (you are) my universe
+And I (I) just want (just want) to put you first
+And you (you), you are (you are) my universe
+And you make my world light up inside
+My universe (doo-doo, doo-doo)
+My universe (doo-doo, doo-doo)
+My universe (doo-doo, doo-doo)
+(You make my world)
+You make my world light up inside
+Make my world light up inside
+나를 밝혀주는 건
+너란 사랑으로 수 놓아진 별
+내 우주의 넌
+또 다른 세상을 만들어 주는 걸
+너는 내 별이자 나의 우주니까
+지금 이 시련도 결국엔 잠시니까
+너는 언제까지나 지금처럼 밝게만 빛나줘
+우리는 너를 따라 이 긴 밤을 수놓을 거야
+너와 함께 날아가 (가)
+When I'm without you, I'm crazy
+자 어서 내 손을 잡아 (아)
+We are made of each other, baby
+You (you), you are (you are) my universe
+And I (I) just want (just want) to put you first
+And you (you), you are (you are) my universe
+And you make my world light up inside
+My universe (you, you are)
+My universe (I just want)
+My universe (you, you are my universe, and I)
+My universe`, youtubeId: "m9BX2yn9524" },
+  { id: 18, title: "A Sky Full of Stars", lyrics: `'Cause you're a sky, 'cause you're a sky full of stars
+I'm gonna give you my heart
+'Cause you're a sky, 'cause you're a sky full of stars
+'Cause you light up the path
+
+I don't care, go on and tear me apart
+I don't care if you do, ooh
+'Cause in a sky, 'cause in a sky full of stars
+I think I saw you
+
+'Cause you're a sky, 'cause you're a sky full of stars
+I wanna die in your arms
+'Cause you get lighter the more it gets dark
+I'm gonna give you my heart
+
+I don't care, go on and tear me apart
+I don't care if you do, ooh
+'Cause in a sky, 'cause in a sky full of stars
+I think I see you
+I think I see you
+
+'Cause you're a sky, you're a sky full of stars
+Such a heavenly view
+You're such a heavenly view`, youtubeId: "NDonh28AY3I" },
   { id: 19, title: "Sunrise", lyrics: "", youtubeId: "H97NQznnvZo" },
-  { id: 20, title: "Sparks", lyrics: "", youtubeId: "n-bMFjAOHxo" },
-  { id: 21, title: "The Jumbotron Song", lyrics: "", youtubeId: "bzRMneypG04" }, // Note: Removed Korean part for simplicity
-  { id: 22, title: "Fix You", lyrics: "", youtubeId: "JJDZZ3Y9GSs" },
-  { id: 23, title: "GOOD FEELINGS", lyrics: "", youtubeId: "DylyIX9gCtA" },
-  { id: 24, title: "feelslikeimfallinginlove", lyrics: "", youtubeId: "2k1142nRRNA" },
+  { id: 20, title: "Sparks", lyrics: `Did I drive you away
+I know what you'll say
+You say, "Oh, sing one you know"
+But I promise you this
+I'll always look out for you
+Yeah that's what I'll do
+
+I say, oh
+I say, oh
+
+My heart is yours
+It's you that I hold on to
+Yeah that's what I do
+And I know I was wrong
+But I won't let you down
+(Oh yeah, oh yeah, oh yeah, I will, yes I will)
+
+I say oh
+I cry oh
+
+Yeah I saw sparks
+Yeah I saw sparks
+And I saw sparks
+Yeah I saw sparks
+
+Sing it out
+
+La, la, la, la, oh
+La, la, la, la, oh
+La, la, la, la, oh`, youtubeId: "n-bMFjAOHxo" },
+  { id: 21, title: "The Jumbotron Song (즉홍곡)", lyrics: "", youtubeId: "" }, // Note: Removed Korean part for simplicity
+  { id: 22, title: "Fix You", lyrics: `When you try your best, but you don't succeed
+When you get what you want, but not what you need
+When you feel so tired, but you can't sleep
+Stuck in reverse
+And the tears come streaming down your face
+When you lose something you can't replace
+When you love someone, but it goes to waste
+Could it be worse?
+
+Lights will guide you home
+And ignite your bones
+And I will try to fix you
+
+And high up above or down below
+When you're too in love to let it go
+But if you never try you'll never know
+Just what you're worth
+
+Lights will guide you home
+And ignite your bones
+And I will try to fix you
+
+Tears stream down your face
+When you lose something you cannot replace
+Tears stream down your face and I
+Tears stream down your face
+I promise you I will learn from my mistakes
+Tears stream down your face and I
+
+Lights will guide you home
+And ignite your bones
+And I will try to fix you`, youtubeId: "bzRMneypG04" },
+  { id: 23, title: "GOOD FEELINGS", lyrics: `Good good
+Oh oh
+All those good good
+Feelings
+All those good good
+Oh oh
+All those good good
+Feelings
+
+We fell in love in the summer
+I remember, baby
+We saw the sunshine too
+And we were born for each other
+Just remember, baby
+Whatever we go through
+
+All the good feelings
+For one another
+As we danced to the radio
+All the good good feelings we have for each other
+Don't ever ever let them go
+
+All the good good feelings
+Don't ever let, don't ever let go
+Don't ever let them go
+Oh
+Don't ever let, don't ever let go
+All those good good feelings
+Don't ever let, don't ever let go
+Don't ever forget those good feelings
+Don't ever let them go
+All those good good
+
+We fell in love in the summer
+I remember, baby
+We fell under the moon
+Oh
+We were born for each other
+I remember when I'm millions of miles from you
+
+All the good feelings
+For one another
+As we danced to the radio
+All the good good feelings we have for each other
+Don't ever ever let them go (Don't ever ever let them go)
+
+All the good good feelings
+Don't ever let, don't ever let go
+Don't ever let them go
+Oh
+Don't ever let, don't ever let go
+All those good good feelings
+Don't ever let, don't ever let go
+Don't ever forget those good feelings
+Don't ever let them go
+All the good good
+Feelings
+
+Hey, the kids want to sing for you
+La la la la la la la
+La la la la
+La la la la la la la
+La la la la
+La la la la la la la
+La la la la
+Oh, we're gonna make it through
+La la la la la la la
+La la la la
+La la la la la la la
+
+All the good good feelings
+Don't ever let, don't ever let go
+Don't ever let them go, oh
+Don't ever let, don't ever let go
+All those good good feelings
+Don't ever let, don't ever let go
+Don't ever let them go, oh
+Don't ever forget those good feelings
+Don't ever let, don't ever let go
+Don't ever let them go
+All the good good feelings
+
+La la la la
+La la la la la la la
+La la la la
+La la la la la la la
+La la la la
+La la la la la la la
+La la la la
+La la la la la la la`, youtubeId: "JJDZZ3Y9GSs" },
+  { id: 24, title: "feelslikeimfallinginlove", lyrics: `I know that this could hurt me bad
+I know that this could feel like that
+But I just can't stop
+Let my defences drop
+I know that I was born to kill
+Any angel on my windowsill
+But it's so dark inside
+I throw the windows wide
+
+I know, la-la-la-la-la-la-la-la-la
+I know, la-la-la-la-la-la-la-la
+Still I don't let go
+And fields of flowers grow
+
+Oh, it feels like
+I'm fallin' in love
+Maybe for the first time
+Baby, it's my mind you blow
+It feels like
+I'm fallin' in love
+You're throwin' me a lifeline
+This is for a lifetime, I know
+
+I know that in this kind of scene
+Of two people there's a spark between
+One gets torn apart
+One gets a broken heart
+
+I know, la-la-la-la-la-la-la-la-la
+I know, la-la-la-la-la-la-la-la
+Still I don't let go
+And fields of flowers grow
+
+Oh, it feels like
+I'm fallin' in love
+Maybe for the first time
+Baby, it's my mind you blow
+It feels like
+I'm fallin' in love
+You're throwin' me a lifeline
+This is for a lifetime, I know
+
+Ooh
+Ooh (Feels like)
+Woah
+
+It feels like
+I'm fallin' in love
+You're throwin' me a lifeline
+Oh, now for the first time
+I know I'm not alone
+
+Ah-la-la, la-la, lo-lee-lo
+Ah-la-la, la-la, lo-lee-lo
+Ah-la-la, la-la, lo-lee-lo, oh
+Ah-la-la, la-la, lo-lee-lo
+Ah-la-la, la-la, lo-lee-lo
+Ah-la-la, la-la, lo-lee-lo, oh`, youtubeId: "DylyIX9gCtA" },
+  { id: 25, title: "Every teardrop is a waterfall", lyrics: `I turn the music up, I got my records on
+I shut the world outside until the lights come on
+Maybe the streets alight, maybe the trees are gone
+I feel my heart start beating to my favourite song
+
+And all the kids they dance, all the kids all night
+Until Monday morning feels another life
+I turn the music up
+I'm on a roll this time
+And heaven is in sight
+
+I turn the music up, I got my records on
+From underneath the rubble sing a rebel song
+Don't want to see another generation drop
+I'd rather be a comma than a full stop
+
+Maybe I'm in the black, maybe I'm on my knees
+Maybe I'm in the gap between the two trapezes
+But my heart is beating and my pulses start
+Cathedrals in my heart
+
+And we saw oh this light I swear you, emerge blinking into
+To tell me it's alright
+As we soar walls, every siren is a symphony
+And every tear's a waterfall
+Is a waterfall
+Oh
+Is a waterfall
+Oh oh oh
+Is a is a waterfall
+Every tear
+Is a waterfall
+Oh oh oh
+
+So you can hurt, hurt me bad
+But still I'll raise the flag
+
+Oh
+It was a wa wa wa wa wa-aterfall
+A wa wa wa wa wa-aterfall
+
+Every tear
+Every tear
+Every teardrop is a waterfall
+
+Every tear
+Every tear
+Every teardrop is a waterfall
+
+Every tear
+Every tear
+Every teardrop is a waterfall
+
+Every tear
+Every tear
+Every teardrop is a waterfall`, youtubeId: "2k1142nRRNA" },
+
 ]; 
